777c965816a11210eede11cc51d8bc6d60b17cfe
erattini <emiliano.rattini@studio.unibo.it>
2025-03-29 16:46:57 +0100

    More functional approach in drawMyNumber controller

--- src/main/scala/u04/drawmynumber/DrawMyNumberState.scala
+++ src/main/scala/u04/drawmynumber/DrawMyNumberState.scala
@@ -9,27 +9,28 @@ enum Result:
 
 trait DrawMyNumberState:
   type DrawMyNumber
-  type Attempts = Int
   def initialState(): DrawMyNumber
   def initialState(topBound: Int, attempts: Int): DrawMyNumber
-  def getTopBoundAndMaxAttempts: State[DrawMyNumber, (Int, Attempts)]
-  def nop(): State[DrawMyNumber, Unit] = State(i => (i, ()))
-  def guess(x: Int): State[DrawMyNumber, (Result, Attempts)]
+  def getTopBoundAndAttempts: State[DrawMyNumber, (Int, Int)]
+  def nop(): State[DrawMyNumber, Unit]
+  def guess(x: Int): State[DrawMyNumber, (Result, Int)]
 
 object DrawMyNumberStateImpl extends DrawMyNumberState:
   import Result.*
-  opaque type DrawMyNumber = (Int, Attempts, (Int, Int))
+  opaque type DrawMyNumber = (Int, Int, (Int, Int))
 
   def initialState(): DrawMyNumber = (new Random().nextInt(100), 10, (100, 10))
   def initialState(topBound: Int, attempts: Int): DrawMyNumber =
     (new Random().nextInt(topBound), attempts, (topBound, attempts))
 
-  def getTopBoundAndMaxAttempts: State[DrawMyNumber, (Int, Attempts)] = State(i => (i, i._3))
+  def getTopBoundAndAttempts: State[DrawMyNumber, (Int, Int)] = State(i => (i, i._3))
+  
+  def nop(): State[DrawMyNumber, Unit] = State(i => (i, ()))
 
-  def guess(x: Int): State[DrawMyNumber, (Result, Attempts)] =
-    State((num, attempts, startingModel) => (num, startingModel) match
-      case (num, (topBound, _)) if x > topBound || x < 0 => ((num, attempts, startingModel), (OutOfBound, topBound))
-      case (num, (topBound, maxAttempts)) if num == x => (initialState(topBound, maxAttempts), (Win, attempts - 1))
-      case (num, (topBound, maxAttempts)) if attempts - 1 == 0 => (initialState(topBound, maxAttempts), (Loss, num))
-      case (num, _) if num < x => ((num, attempts - 1, startingModel), (Less, attempts - 1))
-      case _ => ((num, attempts - 1, startingModel), (More, attempts - 1)))
+  def guess(x: Int): State[DrawMyNumber, (Result, Int)] =
+    State((num, attemptsLeft, startingModel) => (num, startingModel) match
+      case (num, (topBound, _)) if x > topBound || x < 0 => ((num, attemptsLeft, startingModel), (OutOfBound, topBound))
+      case (num, (topBound, maxAttempts)) if num == x => (initialState(topBound, maxAttempts), (Win, attemptsLeft - 1))
+      case (num, (topBound, maxAttempts)) if attemptsLeft - 1 == 0 => (initialState(topBound, maxAttempts), (Loss, num))
+      case (num, _) if num < x => ((num, attemptsLeft - 1, startingModel), (Less, attemptsLeft - 1))
+      case _ => ((num, attemptsLeft - 1, startingModel), (More, attemptsLeft - 1)))
--- src/main/scala/u04/drawmynumber/MVC.scala
+++ src/main/scala/u04/drawmynumber/MVC.scala
@@ -5,39 +5,40 @@ import u04.monads.States.State
 import u04.monads.WindowStateImpl
 import WindowStateImpl.*
 import u04.monads.Event
-import DrawMyNumberStateImpl.{Attempts, getTopBoundAndMaxAttempts, guess, initialState, nop}
+import DrawMyNumberStateImpl.*
 import u04.monads.Monads.Monad.seqN
 import u04.drawmynumber.Result.*
+import u03.extensionmethods.Streams.*
 
 @main def runDrawMyNumber =
-  import u03.extensionmethods.Streams.*
 
-  def mv[SM, SV, AM, AV](m1: State[SM, AM], f: AM => State[SV, AV]): State[(SM, SV), AV] =
-    State: (sm, sv) =>
-      val (sm2, am) = m1.run(sm)
-      val (sv2, av) = f(am).run(sv)
-      ((sm2, sv2), av)
-
-  def windowCreation(topBound: Int, attempts: Attempts): State[Window, Stream[Event]] = for
+  def windowCreation(topBound: Int, attempts: Int): State[Window, Stream[Event]] = for
     _ <- setSize(400, 125)
     _ <- addLabel(text = "You have " + attempts + " attempts. Number is between 0 and " + topBound, name = "Label1")
     _ <- addTextField(name = "GuessField")
-    _ <- addButton(text = "guess", name = "GuessButton", eventInput = "GuessField")
+    _ <- addButton(text = "guess", name = "GuessButton")
     _ <- show()
     events <- eventStream()
   yield events
 
+  def mapToText(res: Any) = {
+    res match
+      case (Win, attempts) => "You won with " + attempts + " attempts left. Guess to play again"
+      case (Loss, number) => "You loss. Number was " + number + ". Guess to play again"
+      case (OutOfBound, topBound) => "Number must be between 0 and " + topBound
+      case (Less, attempts) => "Number is less than that. Attempts left: " + attempts
+      case (More, attempts) => "Number is more than that. Attempts left: " + attempts
+  }
+
+  import u04.monads.mv
   val controller = for
-    events <- mv(getTopBoundAndMaxAttempts, windowCreation)
+    events <- mv(getTopBoundAndAttempts, windowCreation)
     _ <- seqN(events.map {
-      case Event("GuessButton", x) => mv(guess(x.toInt), res => toLabel(res match
-        case (Win, attempts) => "You won with " + attempts + " attempts left. Guess to play again"
-        case (Loss, number) => "You loss. Number was " + number + ". Guess to play again"
-        case (OutOfBound, topBound) => "Number must be between 0 and " + topBound
-        case (Less, attempts) => "Number is less than that. Attempts left: " + attempts
-        case (More, attempts) => "Number is more than that. Attempts left:" + attempts,
-        "Label1"))
+      case Event("GuessButton", _) => for
+        x <- mv(nop(), _ => getText("GuessField"))
+        _ <- mv(guess(x.toInt), res => toLabel(mapToText(res), "Label1"))
+      yield()
     })
   yield ()
 
-  controller.run((initialState(10, 3), initialWindow))
+  controller.run((initialState(100, 8), initialWindow))
